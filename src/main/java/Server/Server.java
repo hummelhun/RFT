3bc97108031395b1a5722953bb8f2c6b3c3d9502
Kozsi <gergo.kozslai@gmail.com>
2017-09-30 15:01:59 +0200

    Created Server.class
    Rewrited the server communication.

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,27 +1,81 @@
 package Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 	
+	public ServerSocket getServerSocket() {
+		return serverSocket;
+	}
+
 	int portNumber;
 	ServerSocket serverSocket;
 	Socket clientSocket1;
 	Socket clientSocket2;
+	PrintWriter out1;
+	PrintWriter out2;
+	BufferedReader in1;
+	BufferedReader in2;
 	
 	public Server(int p){
 		this.portNumber = p;
 	}
 	
-	public void createSocket(){
+	public void Client1InOut() throws IOException{
+		out1 = new PrintWriter(clientSocket1.getOutputStream(), true);
+		in1 = new BufferedReader(new InputStreamReader(clientSocket1.getInputStream()));
+	}
+	public void Client2InOut() throws IOException{
+		out2 = new PrintWriter(clientSocket2.getOutputStream(), true);
+		in2 = new BufferedReader(new InputStreamReader(clientSocket2.getInputStream()));
+	}
+	
+	public void createServerSocket(){
 		try {
-			ServerSocket serverSocket = new ServerSocket(portNumber);
+			this.serverSocket = new ServerSocket(portNumber);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	public void createClientSocket1(){
+		try {
+			this.clientSocket1 = this.serverSocket.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int getPortNumber() {
+		return portNumber;
+	}
+
+	public Socket getClientSocket1() {
+		return clientSocket1;
+	}
+
+	public Socket getClientSocket2() {
+		return clientSocket2;
+	}
+
+	public PrintWriter getOut1() {
+		return out1;
+	}
+
+	public PrintWriter getOut2() {
+		return out2;
+	}
+
+	public BufferedReader getIn1() {
+		return in1;
+	}
+
+	public BufferedReader getIn2() {
+		return in2;
 	}
 	
 }
--- src/main/java/Server/ServerMain.java
+++ src/main/java/Server/ServerMain.java
@@ -1,4 +1,5 @@
 package Server;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,31 +9,30 @@ import java.net.Socket;
 
 public class ServerMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Succesfull");
-		
+
 		if (args.length < 1) {
-		      System.err.println("Usage: java EchoServer <port number>");
-		      System.exit(1);
-		    }
-		    System.out.println("Server started. Listening on Port 8005" );
-		    int portNumber = Integer.parseInt(args[0]);
-		      try (ServerSocket serverSocket = new ServerSocket(portNumber);
-		        Socket clientSocket = serverSocket.accept();
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
-			  System.out.println("Client connected on port " + portNumber +". Servicing requests.");
-			  String inputLine;
-			  while ((inputLine = in.readLine()) != null) {
-		            System.out.println("Received message: " + inputLine + " from " + clientSocket.toString());
-			    out.println(inputLine);
-			  }
-			} catch (IOException e) {
-			    System.out.println("Exception caught when trying to listen on port "
-				    + portNumber + " or listening for a connection");
-		       	}
+			System.err.println("Usage: java EchoServer <port number>");
+			System.exit(1);
+		}
+
+		Server server = new Server(Integer.parseInt(args[0]));
+
+		System.out.println("Server started. Listening on Port 8005");
+		
+		server.createServerSocket();
+		server.createClientSocket1();
+		server.Client1InOut();
+	
+			System.out.println("Client connected on port " + server.getPortNumber() + ". Servicing requests.");
+			String inputLine;
+			while ((inputLine = server.in1.readLine()) != null) {
+				System.out.println("Received message: " + inputLine + " from " + server.clientSocket1.toString());
+				server.out1.println(inputLine);
+			}
+		
 
 	}
 
 }
-

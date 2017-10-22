0982dfda127c9ee99d54528446ef65fcfa87b9a7
gkozslai <gkozslai@rdisoftware.com>
2017-10-22 14:41:34 +0200

    Fixing thread communication.

--- src/main/java/Server/multithread/RequestHandler.java
+++ src/main/java/Server/multithread/RequestHandler.java
@@ -2,6 +2,7 @@ package Server.multithread;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -12,31 +13,54 @@ import java.net.Socket;
 public class RequestHandler implements Runnable {
   private Socket client;
   ServerSocket serverSocket = null;
-
-    public RequestHandler(Socket client) {
+  Server server = new Server();
+  BufferedReader in;
+  BufferedWriter writer;
+  
+    public RequestHandler(Socket client, Server server) {
       this.client = client;
+      this.server = server;
     }
 
     @Override
     public void run() {
-      try ( BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));) {
-	System.out.println("Thread started with name:" + Thread.currentThread().getName());
-	String userInput;
-	while ((userInput = in.readLine()) != null) {
-	  userInput=userInput.replaceAll("[^A-Za-z0-9 ]", "");
-          System.out.println("Received message from " + Thread.currentThread().getName() + " : " + userInput);
-	  writer.write("You entered : " + userInput);
-	  writer.newLine();
-	  writer.flush();
-	}
-      } catch (IOException e) {
-           System.out.println("I/O exception: " + e);
-        } catch (Exception ex) {
-	   System.out.println("Exceprion in Thread Run. Exception : " + ex);
-	  }
+    	try {
+    		writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+    		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			System.out.println("Thread started with name:" + Thread.currentThread().getName());
+			String userInput;
+			while ((userInput = in.readLine()) != null) {
+				  userInput=userInput.replaceAll("[^A-Za-z0-9 ]", "");
+			          System.out.println("Received message from " + Thread.currentThread().getName() + " : " + userInput);
+				  writer.write("You entered : " + userInput);
+				  writer.newLine();
+				  writer.flush();
+				}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 
     }
 
 }
 
+
+
+/*try ( BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));) {
+System.out.println("Thread started with name:" + Thread.currentThread().getName());
+String userInput;
+while ((userInput = in.readLine()) != null) {
+userInput=userInput.replaceAll("[^A-Za-z0-9 ]", "");
+    System.out.println("Received message from " + Thread.currentThread().getName() + " : " + userInput);
+writer.write("You entered : " + userInput);
+writer.newLine();
+writer.flush();
+}
+} catch (IOException e) {
+     System.out.println("I/O exception: " + e);
+  } catch (Exception ex) {
+ System.out.println("Exceprion in Thread Run. Exception : " + ex);
+}*/
 
--- src/main/java/Server/multithread/Server.java
+++ src/main/java/Server/multithread/Server.java
@@ -1,37 +1,48 @@
 package Server.multithread;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Server {
-  public static void main(String[] args) throws IOException {
-    System.out.println("Start of main");
-    if (args.length < 1) {
-      System.err.println("Usage: java EchoServer <port number>");
-      System.exit(1);
-    }
-    int portNumber = Integer.parseInt(args[0]);
-    ExecutorService executor = null;
-    try (ServerSocket serverSocket = new ServerSocket(portNumber);) {
-      executor = Executors.newFixedThreadPool(5);
-      System.out.println("Waiting for clients");
-      while (true) {
-        Socket clientSocket = serverSocket.accept();
-        Runnable worker = new RequestHandler(clientSocket);
-        executor.execute(worker);
-      }
-    }catch (IOException e) {
-       System.out.println("Exception caught when trying to listen on port "
-	+ portNumber + " or listening for a connection");
-       System.out.println(e.getMessage());
-    } finally {
-        if (executor != null) {
-	executor.shutdown();
+	
+	public ServerSocket getServerSocket() {
+		return serverSocket;
 	}
-    }
-  }
-}
 
+	int portNumber;
+	ServerSocket serverSocket;
+	Socket clientSocket;
+
+	public Server(int p){
+		this.portNumber = p;
+	}
+	public Server(){
+		
+	}
+	
+	public void createServerSocket() throws IOException {
+			this.serverSocket = new ServerSocket(portNumber);
+	}
+	
+	public void createClientSocket(){
+		try {
+			this.clientSocket = this.serverSocket.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int getPortNumber() {
+		return portNumber;
+	}
+
+	public Socket getClientSocket() {
+		return clientSocket;
+	}
+
+}
